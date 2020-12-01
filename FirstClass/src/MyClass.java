7563d4a6eb2ee55e340576659ef816affccd5956
Cos Njenga <cosnjenga@gmail.com>
2020-11-30 20:43:33 -0600

    Initial Commit

---
+++ FirstClass/src/MyClass.java
@@ -0,0 +1,15 @@
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+
+		System.out.println("Yes man its cool!");
+
+		int dog = 2;
+		System.out.println(dog);	
+
+	}
+
+}

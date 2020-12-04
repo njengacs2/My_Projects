ae1742478092f66edb69a505843bb95b97d9026d
Cos Njenga <cosnjenga@gmail.com>
2020-12-03 20:30:19 -0600

    Initial Commit

--- P01/src/com/njenga/employee/P01/employee.java
+++ P01/src/com/njenga/employee/P01/employee.java
@@ -1,5 +1,51 @@
 package com.njenga.employee.P01;
 
+import java.util.Scanner;
+
 public class employee {
+
+	private String empName;
+	private Integer empSal;
+
+	public void getName(String en) {
+		//en = "James";
+		//es = 10000;
+		  
+	}
+
+	public void calcSal(Integer es) {
+		System.out.print("===============");
+
+		if(es > 500){
+
+			System.out.print("Employee Salary is 25000K");
+		}
+		else {
+			System.out.print("Employee Salary is 20000K");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner obj = new Scanner(System.in);
+		
+		
+		String name = new String();
+		employee emp = new employee();
+		//emp.getName("James", 1000);
+		
+		System.out.println("enter employee name ");
+		name = obj.nextLine();
+		//name.getName("James");
+		emp.calcSal(2300);
+		
+		
+
+
+
+	}
+
+	
 
 }

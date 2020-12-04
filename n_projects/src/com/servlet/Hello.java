0762e0d9e349228e8bdd432444d105120ce8a7b0
Cos Njenga <cosnjenga@gmail.com>
2020-12-03 21:55:50 -0600

    Initial commit

---
+++ n_projects/src/com/servlet/Hello.java
@@ -0,0 +1,27 @@
+package com.servlet;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Kamau");
+
+		int myFirstNumber = 6;
+		// System.out.println(myFirstNumber);
+
+		int mySecondNumber = 15;
+		// System.out.println(mySecondNumber);
+
+		int myThirdNumber = 18;
+		// System.out.println(myThirdNumber);
+
+		int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+		int myLastOne = 1000 - myTotal;
+
+		System.out.println(myTotal);
+
+		System.out.println(myLastOne);
+
+	}
+
+}
